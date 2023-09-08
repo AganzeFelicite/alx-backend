5ebbd7d4305b96e529a899374c02ac038b85920a
AganzeFelicite <aganzefelicite@gmail.com>
2023-09-08 16:56:00 +0200

    fifo cache implemented

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+this is a task on the
+FIFO caching
+"""
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    this is a class that put and
+    get items stored in a cache
+    in a FIFO order
+    """
+    def __init__(self):
+        """
+        method overloading
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        this adds the key to the
+        dictionary in from the
+        parent class basing on
+        BaseCaching.MAX_ITEMS
+        """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            Key, value = self.cache_data.popitem()
+            print(f"DISCARD:{Key}")
+
+    def get(self, key):
+        """
+        this returns the value
+        related to the key
+        """
+        return self.cache_data[key] or None
