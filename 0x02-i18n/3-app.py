d3a369a0167c5cd41b34f18a069ea60ced5f9482
AganzeFelicite <aganzefelicite@gmail.com>
2023-09-12 23:18:36 +0200

    some translations

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -40,12 +40,7 @@ def index():
     """
     simple hello world
     """
-    home_title = gettext('home_title')
-    home_header = gettext("home_header")
-    result = {'home_title': home_title,
-              'home_header': home_header
-              }
-    return render_template('3-index.html', result=result)
+    return render_template('3-index.html')
 
 
 # if "__main__" == __name__:
