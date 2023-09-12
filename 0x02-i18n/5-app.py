44da363c05f1b4b9aaf1776dd81e917a458c30b4
AganzeFelicite <aganzefelicite@gmail.com>
2023-09-13 00:37:51 +0200

    login mock

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+this is where the app
+is running
+"""
+
+
+from flask import Flask, request, g
+from flask import render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    '''
+    this is a config file
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    determine the best match with our
+    supported languages.
+    """
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+def get_user():
+    """
+    this is a function that
+    checks if the login is
+    passed well
+    """
+    flag = request.args.get('login_as')
+    try:
+        return users.get(int(flag))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """
+    this is a function
+    that runs before all
+    the functioons
+    """
+    g.user = get_user()
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def index():
+    """
+    simple hello world
+    """
+    return render_template('5-index.html')
+
+
+# if "__main__" == __name__:
+#     app.run(debug=True)
