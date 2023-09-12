d000667c7cd36f59a410778a23f0234b53022f9b
AganzeFelicite <aganzefelicite@gmail.com>
2023-09-12 15:11:07 +0200

    second commit

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+this is where the app
+is running
+"""
+
+
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+def config():
+    '''
+    this is a config file
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(config)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def index():
+    """
+    simple hello world
+    """
+    return render_template('1-index.html')
+
+# if "__main__" == __name__:
+#     app.run(debug=True)
